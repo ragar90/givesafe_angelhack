281e9cbf22bc3f7e87dbd59828fa56874e0e73e7
Mark Gatesman <12protons@gmail.com>
2013-06-10 10:58:50 -0400

    Updated fonts Removed accidental "I" from signin screen

--- FoodCircles/src/co/foodcircles/util/C.java
+++ FoodCircles/src/co/foodcircles/util/C.java
@@ -14,6 +14,11 @@ public class C
 	public static Typeface boldFont;
 	public static Typeface italicFont;
 	public static Typeface boldItalicFont;
+	
+	public static Typeface sysFont;
+	public static Typeface sysBoldFont;
+	public static Typeface sysItalicFont;
+	public static Typeface sysBoldItalicFont;
 
 	private static void initFonts(final Context context)
 	{
@@ -23,6 +28,11 @@ public class C
 			boldFont = Typeface.createFromAsset(context.getAssets(), "neutrafaceslabtextbold.ttf");
 			italicFont = Typeface.createFromAsset(context.getAssets(), "neutrafaceslabtextbookitalic.ttf");
 			boldItalicFont = Typeface.createFromAsset(context.getAssets(), "neutrafaceslabtextbolditalic.ttf");
+			
+			sysFont = Typeface.createFromAsset(context.getAssets(), "Roboto-Regular.ttf");
+			sysBoldFont = Typeface.createFromAsset(context.getAssets(), "Roboto-Bold.ttf");
+			sysItalicFont = Typeface.createFromAsset(context.getAssets(), "Roboto-Italic.ttf");
+			sysBoldItalicFont = Typeface.createFromAsset(context.getAssets(), "Roboto-BoldItalic.ttf");
 		}
 	}
 
@@ -44,7 +54,7 @@ public class C
 			else if (v instanceof TextView && !(v instanceof Button) && !(v instanceof EditText))
 			{
 				TextView textView = (TextView) v;
-				
+
 				if ((v.getTag() != null) && ((String) v.getTag()).equals("neu"))
 				{
 					Typeface newFont = font;
@@ -62,6 +72,28 @@ public class C
 						{
 							if (textView.getTypeface().isItalic())
 								newFont = italicFont;
+						}
+					}
+					textView.setTypeface(newFont);
+				}
+				
+				else if ((v.getTag() != null) && ((String) v.getTag()).equals("rob"))
+				{
+					Typeface newFont = sysFont;
+
+					if (textView.getTypeface() != null)
+					{
+						if (textView.getTypeface().isBold())
+						{
+							if (textView.getTypeface().isItalic())
+								newFont = sysBoldItalicFont;
+							else
+								newFont = sysBoldFont;
+						}
+						else
+						{
+							if (textView.getTypeface().isItalic())
+								newFont = sysItalicFont;
 						}
 					}
 					textView.setTypeface(newFont);
