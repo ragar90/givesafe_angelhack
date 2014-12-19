cc005b279caeacb8ed58f2a85dcf85354ec57ac0
matuzenko88 <omatuzenko@gmail.com>
2014-12-19 09:36:53 +0200

    Added new https URL

--- FoodCircles-AndroidStudio/foodCircles/src/main/java/co/foodcircles/net/Net.java
+++ FoodCircles-AndroidStudio/foodCircles/src/main/java/co/foodcircles/net/Net.java
@@ -1,15 +1,11 @@
 package co.foodcircles.net;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import android.annotation.SuppressLint;
+import android.util.Log;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -18,8 +14,17 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.protocol.HttpContext;
@@ -28,17 +33,26 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.annotation.SuppressLint;
-import android.util.Log;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.List;
+
 import co.foodcircles.exception.NetException2;
 import co.foodcircles.json.Charity;
 import co.foodcircles.json.Reservation;
 import co.foodcircles.json.Venue;
 import co.foodcircles.json.Voucher;
+import co.foodcircles.util.MySSLSocketFactory;
 
 
 public class Net {
-	public static final String HOST = "http://staging.foodcircles.net";
+	//public static final String HOST = "http://staging.foodcircles.net";
+    public static final String HOST = "https://joinfoodcircles.org";
 	private static final String API_URL = "/api";
 	private static final String GET_VENUES = "/venues/%f/%f";
 	private static final String GET_RESERVATION = "/reservations/[reservationId]";
@@ -46,17 +60,19 @@ public class Net {
 	private static final String GET_NEWS = "/news";
 	private static final String MARK_REDEEMED = "/payment/used?code=";
 	private static final String GET_TIMELINE="/timeline?auth_token=%s";
-	public static String logo="http://staging.foodcircles.net/media/BAhbBlsHOgZmSSIkMjAxMy8wOC8yMC8xNl81Nl8xMV84MzNfRkFRLnBuZwY6BkVU";
-	public static String website="http://staging.foodcircles.net";
+    private static final String VENUES_SUBSCRIBE = "/venues/%s/subscribe";
+	//public static String logo="http://staging.foodcircles.net/media/BAhbBlsHOgZmSSIkMjAxMy8wOC8yMC8xNl81Nl8xMV84MzNfRkFRLnBuZwY6BkVU";
+    public static String logo="https://joinfoodcircles.org/media/BAhbBlsHOgZmSSIkMjAxMy8wOC8yMC8xNl81Nl8xMV84MzNfRkFRLnBuZwY6BkVU";
 	
 	private static String post(String path, List<BasicNameValuePair> postValues) {
 		HttpContext httpContext = new BasicHttpContext();
-		HttpClient httpclient = new DefaultHttpClient();
+		HttpClient httpclient = createHttpClient();
 		HttpPost httppost = new HttpPost(HOST + API_URL + path);
 		String response = "";
 
 		try {
-			httppost.setEntity(new UrlEncodedFormEntity(postValues, HTTP.UTF_8));
+            if (postValues != null)
+			    httppost.setEntity(new UrlEncodedFormEntity(postValues, HTTP.UTF_8));
 			httppost.setHeader("Content-Type", "application/x-www-form-urlencoded");
 			HttpResponse httpResp = httpclient.execute(httppost, httpContext);
 			response = EntityUtils.toString(httpResp.getEntity());
@@ -69,7 +85,7 @@ public class Net {
 
 	private static String postRedeemed(String id) {
 		String responseString = null;
-		HttpClient httpclient = new DefaultHttpClient();
+		HttpClient httpclient = createHttpClient();
 	    HttpResponse response = null;
 		try {
 			response = httpclient.execute(new HttpGet(HOST + MARK_REDEEMED + id));
@@ -88,7 +104,7 @@ public class Net {
 	
 	private static String get(String path, List<BasicNameValuePair> urlParams) {
 	    StringBuilder builder = new StringBuilder();
-		HttpClient httpclient = new DefaultHttpClient();
+		HttpClient httpclient = createHttpClient();
 		HttpGet httpGet = new HttpGet(HOST + API_URL + path);
 
 		try {
@@ -114,7 +130,7 @@ public class Net {
 	
 	private static String put(String path, List<BasicNameValuePair> urlParams) {
 		HttpContext httpContext = new BasicHttpContext();
-		HttpClient httpclient = new DefaultHttpClient();
+		HttpClient httpclient = createHttpClient();
 		HttpPut httpput = new HttpPut(HOST + API_URL + path);
 		String response = "";
 
@@ -130,7 +146,31 @@ public class Net {
 		return response;
 	}
 
-	@SuppressLint("DefaultLocale")
+    public static HttpClient createHttpClient() {
+        try {
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(null, null);
+
+            SSLSocketFactory sf = new MySSLSocketFactory(trustStore);
+            sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+            HttpParams params = new BasicHttpParams();
+            HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+            HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+            SchemeRegistry registry = new SchemeRegistry();
+            registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+            registry.register(new Scheme("https", sf, 443));
+
+            ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+
+            return new DefaultHttpClient(ccm, params);
+        } catch (Exception e) {
+            return new DefaultHttpClient();
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
 	public static List<Venue> getVenues(double longitude,double latitude,List<BasicNameValuePair> filters)
 			throws NetException {
 		try {
@@ -294,7 +334,7 @@ public class Net {
 		try {
 			json = new JSONObject(html);
 			if (json.getBoolean("error") == true) {
-				n.setMessage(json.getString("description"));
+				n.setMessage(getSignUpErrorMsg(json));
 				throw n;
 			}
 			return json.getString("auth_token");
@@ -415,4 +455,26 @@ public class Net {
 			return "Many";
 		}
 	}
+
+    public static String subscribeVenue(String id) {
+        String html = post(String.format("/venues/%s/subscribe", id), null);
+        return null;
+    }
+
+    public static String unsubscribeVenue(String id) {
+        String html = post(String.format("/venues/%s/unsubscribe", id), null);
+        return null;
+    }
+
+    public static boolean isSubscribed(String id) throws NetException{
+        String html = get(String.format("/venues/%s/subscribed.json", id), null);
+        boolean isSubscribed = false;
+        try {
+            JSONObject jsonObject=new JSONObject(html);
+            isSubscribed = jsonObject.optBoolean("subscribed", false);
+        } catch (JSONException e) {
+            throw new NetException();
+        }
+        return isSubscribed;
+    }
 }
